3a8df4cc243f0edadf301b5f5105b271b708c6f2
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-05 17:03:28 +0530

    started services section

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,6 +2,53 @@ import React from 'react'
 
 export const Services = () => {
   return (
-    <div>Services</div>
+    <div className='p-8 w-full'>
+      <div>
+        <h1 className='font-semibold text-5xl text-transparent bg-gradient-to-r from-red-700 from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text text-center'>What does Resnet Offer ?</h1>
+        <div className='flex flex-col items-center mt-4'>
+          <div>How we help you move through <span className='font-semibold text-red-500'>Machine Learning</span> and</div>
+          <div className='font-semibold text-red-500'>
+            Artificial Intelligence
+          </div>
+        </div>
+      </div>
+
+      <div className='relative flex py-20 w-full h-[100vh]'>
+
+        <div className='left-slides w-[50%] relative'>
+
+          {/* <div className='content-slide top-left-slide'>
+            <h2>
+              We Do the hardwork for you
+            </h2>
+            <p className=''>
+            1st slide 1st slide 1st slide business with AI and ML? Contact us today for a consultation. Our team is eager to understand your unique challenges and design a tailor-made solution that propels your business into the future.vv
+            </p>
+          </div>
+
+          <div className='content-slide middle-slide'>
+            <h2>
+              We Do the hardwork for you
+            </h2>
+            <p className=''>
+            2nd slide 2nd slide 2nd slide businessor-made solution that propels your business into the future.vv
+            </p>
+          </div>
+          
+          <div className='content-slide bottom-left-slide'>
+            <h2>
+              We Do the hardwork for you
+            </h2>
+            <p className=''>
+            3rd slide 3rd slide 3rd slide Ready to elevate your business with AI and ML? Contact us today for a consultation. Our team is eag
+            </p>
+          </div> */}
+
+
+        </div>
+
+
+      </div>
+    </div>
   )
 }
