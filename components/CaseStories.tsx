8431c40b6e3e16fdfcb44af7e792ebd580d3d56d
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-07 01:58:34 +0530

    selling section

--- components/CaseStories.tsx
+++ components/CaseStories.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+'use client'
+
+import React,{useState} from 'react'
 import Image from 'next/image'
 import '../styles/CaseStories.css'
 import cardImg from '../assets/caseStudy-card.png'
-
+import closeImg from '../assets/x-circle-fill.svg'
 export const CaseStories = () => {
+
+  const [clicked0,setClicked0] = useState(false)
+  const [clicked1,setClicked1] = useState(false)
+  const [clicked2,setClicked2] = useState(false)
+  const [clicked3,setClicked3] = useState(false)
+
+  const cardClicked = (index:number)=>{
+    setClicked0(false);
+    setClicked1(false);
+    setClicked2(false);
+    setClicked3(false);
+
+    if(index===0){
+      setClicked0(true);
+    }
+    else if(index===1){
+      setClicked1(true);
+    }
+    else if(index===2){
+      setClicked2(true);
+    }
+    else if(index===3){
+      setClicked3(true);
+    }
+  }
+
   return (
     <div className='py-8 w-full'>
       <div>
@@ -23,14 +51,24 @@ export const CaseStories = () => {
         <div className='relative w-[80%] left-[8%]'>
 
         
-        <div className="flip-card h-[450px] w-[30%] rounded-xl absolute left-[0%] cursor-pointer">
+        <div onClick={()=>cardClicked(0)} className={clicked0?' h-[450px] w-[30%] rounded-xl absolute card-clicked':`flip-card h-[450px] w-[30%] rounded-xl absolute left-[0%] cursor-pointer`}>
 
           <div className="flip-card-inner h-[450px] w-full rounded-xl">
 
-            <div className="flip-card-front h-[450px] w-full rounded-xl">
+            <div className=" flip-card-front h-[450px] w-full rounded-xl" style={clicked0?{display:'none'}:{}}>
               <Image src={cardImg} alt='Card' className='w-full h-full rounded-xl object-cover'></Image>
             </div>
-            <div className="flip-card-back rounded-xl">
+
+            <div className="flip-card-back rounded-xl relative" style={clicked0?{display:'block'}:{}}>
+              {
+                clicked0?
+                <div className='z-100' onClick={(e) => { e.stopPropagation(); setClicked0(!clicked0); }}>
+                  <Image src={closeImg} alt='X' className='absolute right-[2%] top-[2%] z-12 cursor-pointer'></Image>
+
+                </div>
+                :''
+                
+              }
               <h1 className='text-4xl font-bold'>Document Automation</h1>
               <p className='mt-6 text-justify'>
               Implementing Artificial Intelligence (AI) for documentation automation addresses manual inefficiencies by streamlining tasks, reducing errors, and fostering collaboration. It ensures scalability, diminishes reliance on experts, and contributes to environmental sustainability. The shift to AI signifies a transformative leap towards heightened productivity, precision, and eco-friendly information management.
@@ -39,41 +77,67 @@ export const CaseStories = () => {
 
           </div>
         </div>
-        <div className="flip-card h-[450px] w-[30%] rounded-xl absolute left-[25%] cursor-pointer">
+        <div onClick={()=>cardClicked(1)} className={clicked1?' h-[450px] w-[30%] rounded-xl absolute  left-[25%] cursor-pointer card-clicked':`flip-card h-[450px] w-[30%] rounded-xl absolute left-[25%] cursor-pointer`}>
           <div className="flip-card-inner h-[450px] w-full rounded-xl">
 
-            <div className="flip-card-front h-[450px] w-full rounded-xl">
+            <div className="flip-card-front h-[450px] w-full rounded-xl" style={clicked1?{display:'none'}:{}}>
               <Image src={cardImg} alt='Card' className='w-full h-full rounded-xl object-cover'></Image>
             </div>
-            <div className="flip-card-back rounded-xl">
+            <div className="flip-card-back rounded-xl" style={clicked0?{display:'block'}:{}}>
+              {
+                clicked1?
+                <div className='z-100' onClick={(e) => { e.stopPropagation(); setClicked1(!clicked1); }}>
+                  <Image src={closeImg} alt='X' className='absolute right-[2%] top-[2%] z-12 cursor-pointer'></Image>
+
+                </div>
+                :''
+                
+              }
               <h1>AI for Data Analysis</h1>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quia eos laborum explicabo excepturi sunt fuga possimus aut, ad ab. Atque consequuntur necessitatibus totam voluptatibus debitis tempora amet temporibus veniam ex, id quas minima fugiat consequatur autem rem quam qui maxime vero reprehenderit libero? Reprehenderit, delectus.</p>
             </div>
 
           </div>
         </div>
-        <div className="flip-card h-[450px] w-[30%] rounded-xl absolute left-[50%] cursor-pointer">
+        <div onClick={()=>cardClicked(2)} className={clicked2?' h-[450px] w-[30%] rounded-xl absolute left-[50%] cursor-pointer card-clicked ':`flip-card h-[450px] w-[30%] rounded-xl absolute left-[50%] cursor-pointer`}>
           <div className="flip-card-inner h-[450px] w-full rounded-xl">
 
-            <div className="flip-card-front h-[450px] w-full rounded-xl">
+            <div className="flip-card-front h-[450px] w-full rounded-xl" style={clicked2?{display:'none'}:{}}>
               <Image src={cardImg} alt='Card' className='w-full h-full rounded-xl object-cover'></Image>
             </div>
-            <div className="flip-card-back rounded-xl">
-              <h1>AI in Healthcare</h1>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur minima obcaecati, aut dolorem enim omnis laudantium quae, doloribus accusantium porro quam corporis ut modi quasi voluptates neque asperiores at ea! Sint, animi. Provident magni libero ipsum eveniet error consequuntur maxime.</p>
+            <div className="flip-card-back rounded-xl" style={clicked2?{display:'block'}:{}}>
+              {
+                clicked2?
+                <div className='z-100' onClick={(e) => { e.stopPropagation(); setClicked2(!clicked2); }}>
+                  <Image src={closeImg} alt='X' className='absolute right-[2%] top-[2%] z-12 cursor-pointer'></Image>
+
+                </div>
+                :''
+                
+              }
+              <h1>AI for Data Analysis</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quia eos laborum explicabo excepturi sunt fuga possimus aut, ad ab. Atque consequuntur necessitatibus totam voluptatibus debitis tempora amet temporibus veniam ex, id quas minima fugiat consequatur autem rem quam qui maxime vero reprehenderit libero? Reprehenderit, delectus.</p>
             </div>
 
           </div>
         </div>
-        <div className="flip-card h-[450px] w-[30%] rounded-xl absolute left-[75%] cursor-pointer">
+        <div onClick={()=>cardClicked(3)} className={clicked3?' h-[450px] w-[30%] rounded-xl absolute left-[75%] cursor-pointer card-clicked':`flip-card h-[450px] w-[30%] rounded-xl absolute left-[75%] cursor-pointer`}>
           <div className="flip-card-inner h-[450px] w-full rounded-xl">
 
-            <div className="flip-card-front h-[450px] w-full rounded-xl">
+            <div className="flip-card-front h-[450px] w-full rounded-xl" style={clicked3?{display:'none'}:{}}>
               <Image src={cardImg} alt='Card' className='w-full h-full rounded-xl object-cover'></Image>
             </div>
-            <div className="flip-card-back rounded-xl">
-              <h1>Job Automation</h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt vel esse voluptates provident sit id, aut perferendis adipisci tempora nihil. Sint nihil iure modi omnis obcaecati quia. Distinctio nobis reprehenderit dicta aut et suscipit animi fuga.</p>
+            <div className="flip-card-back rounded-xl" style={clicked3?{display:'block'}:{}}>
+              {
+                clicked3?
+                <div className='z-100' onClick={(e) => { e.stopPropagation(); setClicked3(!clicked3); }}>
+                  <Image src={closeImg} alt='X' className='absolute right-[2%] top-[2%] z-12 cursor-pointer'></Image>
+
+                </div>
+                :''
+              }
+              <h1>AI for Data Analysis</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quia eos laborum explicabo excepturi sunt fuga possimus aut, ad ab. Atque consequuntur necessitatibus totam voluptatibus debitis tempora amet temporibus veniam ex, id quas minima fugiat consequatur autem rem quam qui maxime vero reprehenderit libero? Reprehenderit, delectus.</p>
             </div>
 
           </div>
--- components/SellingSection.tsx
+++ components/SellingSection.tsx
@@ -1,7 +1,96 @@
-import React from 'react'
+'use client'
 
+import React ,{useEffect,useState} from 'react'
+import '../styles/Selling.css'
 export const SellingSection = () => {
+
+
+const [currentIndex, setCurrentIndex] = useState(0);
+const contentsLeft = [
+  {
+    "heading": "10+ Years",
+    "content": "With over 1 decade of dedicated service in the AI and Machine Learning industry, we've honed our expertise, refined our strategies, and continuously evolved to meet the dynamic needs of our clients"
+  },
+  {
+    "heading": "Heading 2",
+    "content": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    "heading": "Heading 3",
+    "content": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  },
+  {
+    "heading": "Heading 4",
+    "content": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+];
+const contentsRight = [
+  {
+    "heading": "365 Days",
+    "content": "With over 1 decade of dedicated service in the AI and Machine Learning industry, we've honed our expertise, refined our strategies, and continuously evolved to meet the dynamic needs of our clients"
+  },
+  {
+    "heading": "Heading 2",
+    "content": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    "heading": "Heading 3",
+    "content": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  },
+  {
+    "heading": "Heading 4",
+    "content": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % contentsLeft.length);
+    }, 100000); // Change the interval as needed
+
+    return () => clearInterval(interval);
+  }, []);
   return (
-    <div>SellingSection</div>
+    <div className='relative w-full py-20'>
+
+      <h1 className='font-semibold text-5xl text-transparent bg-gradient-to-r from-red-700 from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text text-center'>People Love Us</h1>
+
+      <p className='w-[40%] mt-4 text-center absolute left-[50%] -translate-x-[50%]'>A decade od excellence , <span className='font-semibold text-red-500'>70 +</span> Clients Strong - Celebrating a <span className='font-semibold text-red-500'>Decade</span> of Trust and Innovation in the AI & Machine Learning Industry!</p>
+
+
+      <div className='mt-20 flex justify-center w-full'>
+
+        <div className='p-4 pr-6 h-[32vh] w-[30%] relative overflow-hidden '>
+          {
+            contentsLeft.map((element,index)=>{
+              return (
+                <div className={`content wordspin h-[32vh] ${index===currentIndex?'active':'not-active'} `} key={index} >
+                  <h2 className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#39A94B] from-10% via-[#6cff85] via-40% to-[#00FFFF] to-100% font-extrabold text-end pb-6'>{element.heading}</h2>
+                  <p className='text-xl text-white text-end'>
+                  {element.content}
+                  </p>
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className='pl-6 p-4 border-s-[3px] border-gray-700 h-[35vh] w-[30%] relative overflow-hidden'>
+          {
+            contentsRight.map((element,index)=>{
+              return (
+                <div className={`content wordspin h-[32vh] ${index===currentIndex?'active':'not-active'} `} key={index} >
+                  <h2 className='text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#00FFFF]  to-[#39A94B] font-extrabold text-start pb-6'>{element.heading}</h2>
+                  <p className='text-xl text-white text-start'>
+                  {element.content}
+                  </p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+
+    </div>
   )
 }
