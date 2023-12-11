b9d46e0660d9feaa593972e3924c64ae421df39c
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-12-11 22:48:57 +0530

    added service

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,92 +1,223 @@
-'use client'
+"use client";
 
-import React,{useEffect} from 'react'
-import '../styles/Services.css'
+import React, { useEffect } from "react";
+import "../styles/Services.css";
 
-import image1 from '../assets/services-image1.png'
-import image2 from '../assets/services-image2.png'
-import image3 from '../assets/services-image3.png'
-import image4 from '../assets/services-image4.png'
+import image1 from "../assets/s1.jpg";
+import image2 from "../assets/s2.jpg";
+import image3 from "../assets/s3.jpg";
+import image4 from "../assets/s4.jpg";
 
-import Image from 'next/image'
+import Image from "next/image";
 export const Services = () => {
+  // return (
+  //   <div className='p-8 w-full mt-20 relative h-[140vh]'>
+  //     <div>
+  //       <h1 className='font-semibold text-5xl text-transparent bg-gradient-to-r from-red-700 from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text text-center'>What does Resnet Offer ?</h1>
+  //       <div className='flex flex-col items-center mt-4'>
+  //         <div>How we help you move through <span className='font-semibold text-red-500'>Machine Learning</span> and</div>
+  //         <div className='font-semibold text-red-500'>
+  //           Artificial Intelligence
+  //         </div>
+  //       </div>
+  //     </div>
 
+  //     <div className='left-0 absolute top-[-5%] flex gap-24 w-full h-[140vh] overflow-hidden'>
+
+  //       <div className='left-slides w-[50%] relative'>
+
+  //         <div className='content-slide c-slide-top'>
+  //           <h2>
+  //             We Do the hardwork for you
+  //           </h2>
+  //           <p className=''>
+  //           1st slide 1st slide 1st slide business with AI and ML? Contact us today for a consultation. Our team is eager to understand your unique challenges and design a tailor-made solution that propels your business into the future.vv
+  //           </p>
+  //         </div>
+
+  //         <div className='content-slide c-slide-right'>
+  //           <h2>
+  //             We Do the hardwork for you
+  //           </h2>
+  //           <p className=''>
+  //           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, excepturi a iste omnis accusamus quia illo illum molestias aliquid voluptate sapiente possimus nemo ipsum distinctio minima delectus. Cupiditate illum dolor qui error!
+  //           </p>
+  //         </div>
+
+  //         <div className='content-slide c-slide-bottom'>
+  //           <h2>
+  //             We Do the hardwork for you
+  //           </h2>
+  //           <p className=''>
+  //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus dolorum est ipsum architecto ut eum aut eaque voluptatibus assumenda velit recusandae nostrum nam quisquam distinctio tempore laboriosam magni, sint obcaecati repudiandae error!
+  //           </p>
+  //         </div>
+  //         <div className='content-slide c-slide-left'>
+  //           <h2>
+  //             We Do the hardwork for you
+  //           </h2>
+  //           <p className=''>
+  //           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, dignissimos voluptatibus voluptates temporibus illo harum est praesentium. Minus accusantium reiciendis similique molestias fugit, dolore quia dignissimos quis et consectetur blanditiis ea.
+  //           </p>
+  //         </div>
+
+  //       </div>
+
+  //       <div className='w-[50%] relative right-slides'>
+
+  //         <div className='right-slide slide-top'>
+  //           <Image src={image1} alt='.' ></Image>
+  //         </div>
+  //         <div className='right-slide slide-left'>
+  //           <Image src={image2} alt='.' ></Image>
+  //         </div>
+  //         <div className='right-slide slide-bottom'>
+  //           <Image src={image3} alt='.' ></Image>
+  //         </div>
+  //         <div className='right-slide slide-right'>
+  //           <Image src={image4} alt='.' ></Image>
+  //         </div>
+
+  //       </div>
+
+  //     </div>
+  //   </div>
+  // )
   return (
-    <div className='p-8 w-full mt-20 relative h-[140vh]'>
-      <div>
-        <h1 className='font-semibold text-5xl text-transparent bg-gradient-to-r from-red-700 from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text text-center'>What does Resnet Offer ?</h1>
-        <div className='flex flex-col items-center mt-4'>
-          <div>How we help you move through <span className='font-semibold text-red-500'>Machine Learning</span> and</div>
-          <div className='font-semibold text-red-500'>
-            Artificial Intelligence
-          </div>
-        </div>
-      </div>
-       
-      <div className='left-0 absolute top-[-5%] flex gap-24 w-full h-[140vh] overflow-hidden'>
-
-        <div className='left-slides w-[50%] relative'>
-
-          
-          <div className='content-slide c-slide-top'>
-            <h2>
-              We Do the hardwork for you
-            </h2>
-            <p className=''>
-            1st slide 1st slide 1st slide business with AI and ML? Contact us today for a consultation. Our team is eager to understand your unique challenges and design a tailor-made solution that propels your business into the future.vv
-            </p>
-          </div>
-          
-
-          
-          <div className='content-slide c-slide-right'>
-            <h2>
-              We Do the hardwork for you
-            </h2>
-            <p className=''>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, excepturi a iste omnis accusamus quia illo illum molestias aliquid voluptate sapiente possimus nemo ipsum distinctio minima delectus. Cupiditate illum dolor qui error!
-            </p>
-          </div>
-          
-          <div className='content-slide c-slide-bottom'>
-            <h2>
-              We Do the hardwork for you
-            </h2>
-            <p className=''>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus dolorum est ipsum architecto ut eum aut eaque voluptatibus assumenda velit recusandae nostrum nam quisquam distinctio tempore laboriosam magni, sint obcaecati repudiandae error!
-            </p>
-          </div>
-          <div className='content-slide c-slide-left'>
-            <h2>
-              We Do the hardwork for you
-            </h2>
-            <p className=''>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, dignissimos voluptatibus voluptates temporibus illo harum est praesentium. Minus accusantium reiciendis similique molestias fugit, dolore quia dignissimos quis et consectetur blanditiis ea.
+    <div className="flex flex-col justify-center">
+      <section>
+        <div className="container">
+          <div className="parent-circle">
+            <h2 className="child">User Experience</h2>
+            <h3 className="child">We are all users</h3>
+            <Image
+              alt="User Experience"
+              src={image1}
+              className="image w-[170px] aspect-square"
+              width={270}
+              height={270}
+            />
+            <p className="service-details child">
+              <span>
+                With over 200 digital projects and counting, we get what users
+                want and need pretty easily. UX is the core to everything we
+                build. In Codigo, we take user research, competitive analysis,
+                wireframes, user flows, content maps very seriously.
+              </span>{" "}
             </p>
           </div>
 
+          <h4 className="main-tag">User Experience</h4>
 
+          <p className="main-tag">How users interact</p>
         </div>
 
-        <div className='w-[50%] relative right-slides'>
+        <div className="container">
+          <div className="parent-circle">
+            <h2 className="child">User Experience</h2>
+            <h3 className="child">We are all users</h3>
+            <Image
+              alt="User Experience"
+              src={image2}
+              className="image w-[170px] aspect-square"
+              width={200}
+              height={200}
+            />
+            <p className="service-details child">
+              <span>
+                With over 200 digital projects and counting, we get what users
+                want and need pretty easily. UX is the core to everything we
+                build. In Codigo, we take user research, competitive analysis,
+                wireframes, user flows, content maps very seriously.
+              </span>{" "}
+            </p>
+          </div>
 
-          <div className='right-slide slide-top'>
-            <Image src={image1} alt='.' ></Image>
-          </div>
-          <div className='right-slide slide-left'>
-            <Image src={image2} alt='.' ></Image>
-          </div>
-          <div className='right-slide slide-bottom'>
-            <Image src={image3} alt='.' ></Image>
-          </div>
-          <div className='right-slide slide-right'>
-            <Image src={image4} alt='.' ></Image>
-          </div>
+          <h4 className="main-tag">User Experience</h4>
 
+          <p className="main-tag">How users interact</p>
         </div>
 
+        <div className="container">
+          <div className="parent-circle">
+            <h2 className="child">User Experience</h2>
+            <h3 className="child">We are all users</h3>
+            <Image
+              alt="User Experience"
+              src={image3}
+              className="image w-[170px] aspect-square "
+              width={200}
+              height={200}
+            />
+            <p className="service-details child">
+              <span>
+                With over 200 digital projects and counting, we get what users
+                want and need pretty easily. UX is the core to everything we
+                build. In Codigo, we take user research, competitive analysis,
+                wireframes, user flows, content maps very seriously.
+              </span>{" "}
+            </p>
+          </div>
 
-      </div>
+          <h4 className="main-tag">User Experience</h4>
+
+          <p className="main-tag">How users interact</p>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="parent-circle">
+            <h2 className="child">User Experience</h2>
+            <h3 className="child">We are all users</h3>
+            <Image
+              alt="User Experience"
+              src={image4}
+              className="image"
+              width={150}
+              height={150}
+            />
+            <p className="service-details child">
+              <span>
+                With over 200 digital projects and counting, we get what users
+                want and need pretty easily. UX is the core to everything we
+                build. In Codigo, we take user research, competitive analysis,
+                wireframes, user flows, content maps very seriously.
+              </span>{" "}
+            </p>
+          </div>
+
+          <h4 className="main-tag">User Experience</h4>
+
+          <p className="main-tag">How users interact</p>
+        </div>
+
+        <div className="container">
+          <div className="parent-circle">
+            <h2 className="child">User Experience</h2>
+            <h3 className="child">We are all users</h3>
+            <Image
+              alt="User Experience"
+              src={image4}
+              className="image"
+              width={150}
+              height={150}
+            />
+            <p className="service-details child">
+              <span>
+                With over 200 digital projects and counting, we get what users
+                want and need pretty easily. UX is the core to everything we
+                build. In Codigo, we take user research, competitive analysis,
+                wireframes, user flows, content maps very seriously.
+              </span>{" "}
+            </p>
+          </div>
+
+          <h4 className="main-tag">User Experience</h4>
+
+          <p className="main-tag">How users interact</p>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -19,16 +19,16 @@ const TestimonialCard = ({data }:any)=>{
     <div className=''>
     <div className='relative max-w-[913px] mt-[50px] min-h-[220px] h-full  mx-auto  w-full' style={{boxSizing :"content-box"}}>
       <svg className='absolute -top-10 -left-10 w-[60px] h-[50px] md:h-auto md:w-auto' xmlns="http://www.w3.org/2000/svg" width="120" height="100" viewBox="0 0 120 100" fill="none">
-        <path d="M120 53.9386L90.8946 53.9386C91.3921 30.0989 97.4085 27.3927 104.965 26.6203L107.879 26.2143L107.879 0.000151397L104.52 0.202444C94.6527 0.826368 83.7436 2.8255 76.4655 12.9233C70.0855 21.7754 67.2727 36.2372 67.2727 58.4375L67.2727 99.5747L120 99.5747L120 53.9386Z" fill="url(#paint0_linear_34_181)" fill-opacity="0.46" />
-        <path d="M52.7273 99.5747L52.7273 53.9386L24.0103 53.9386C24.5079 30.0989 30.3297 27.3927 37.8861 26.6203L40.6061 26.2143L40.6061 0.000151397L37.4412 0.202452C27.574 0.826368 16.5673 2.8255 9.28912 12.9233C2.90972 21.7754 2.7815e-05 36.2372 2.60881e-05 58.4375L2.28882e-05 99.5747L52.7273 99.5747Z" fill="url(#paint1_linear_34_181)" fill-opacity="0.46" />
+        <path d="M120 53.9386L90.8946 53.9386C91.3921 30.0989 97.4085 27.3927 104.965 26.6203L107.879 26.2143L107.879 0.000151397L104.52 0.202444C94.6527 0.826368 83.7436 2.8255 76.4655 12.9233C70.0855 21.7754 67.2727 36.2372 67.2727 58.4375L67.2727 99.5747L120 99.5747L120 53.9386Z" fill="url(#paint0_linear_34_181)" fillOpacity="0.46" />
+        <path d="M52.7273 99.5747L52.7273 53.9386L24.0103 53.9386C24.5079 30.0989 30.3297 27.3927 37.8861 26.6203L40.6061 26.2143L40.6061 0.000151397L37.4412 0.202452C27.574 0.826368 16.5673 2.8255 9.28912 12.9233C2.90972 21.7754 2.7815e-05 36.2372 2.60881e-05 58.4375L2.28882e-05 99.5747L52.7273 99.5747Z" fill="url(#paint1_linear_34_181)" fillOpacity="0.46" />
         <defs>
           <linearGradient id="paint0_linear_34_181" x1="93.6364" y1="99.5747" x2="93.6364" y2="0.000149998" gradientUnits="userSpaceOnUse">
             <stop />
-            <stop offset="1" stop-color="white" stop-opacity="0.65" />
+            <stop offset="1" stopColor="white" stopOpacity="0.65" />
           </linearGradient>
           <linearGradient id="paint1_linear_34_181" x1="26.3637" y1="99.5747" x2="26.3637" y2="0.000149998" gradientUnits="userSpaceOnUse">
             <stop />
-            <stop offset="1" stop-color="white" stop-opacity="0.65" />
+            <stop offset="1" stopColor="white" stopOpacity="0.65" />
           </linearGradient>
         </defs>
       </svg>
@@ -38,16 +38,16 @@ const TestimonialCard = ({data }:any)=>{
       </p>
 
       <svg className='absolute -z-50 bottom-0 md:bottom-10 -right-10 w-[60px] h-[50px] md:h-auto md:w-auto' xmlns="http://www.w3.org/2000/svg" width="120" height="100" viewBox="0 0 120 100" fill="none">
-        <path d="M0 45.6361H29.1055C28.6079 69.4758 22.5915 72.182 15.0352 72.9544L12.1212 73.3604V99.5746L15.48 99.3723C25.3473 98.7483 36.2564 96.7492 43.5345 86.6514C49.9145 77.7993 52.7273 63.3375 52.7273 41.1372V0H0V45.6361Z" fill="url(#paint0_linear_34_175)" fill-opacity="0.46" />
-        <path d="M67.2727 0V45.6361H95.9897C95.4921 69.4758 89.6703 72.182 82.1139 72.9544L79.3939 73.3604V99.5746L82.5588 99.3723C92.426 98.7483 103.433 96.7492 110.711 86.6514C117.09 77.7993 120 63.3375 120 41.1372V0H67.2727Z" fill="url(#paint1_linear_34_175)" fill-opacity="0.46" />
+        <path d="M0 45.6361H29.1055C28.6079 69.4758 22.5915 72.182 15.0352 72.9544L12.1212 73.3604V99.5746L15.48 99.3723C25.3473 98.7483 36.2564 96.7492 43.5345 86.6514C49.9145 77.7993 52.7273 63.3375 52.7273 41.1372V0H0V45.6361Z" fill="url(#paint0_linear_34_175)" fillOpacity="0.46" />
+        <path d="M67.2727 0V45.6361H95.9897C95.4921 69.4758 89.6703 72.182 82.1139 72.9544L79.3939 73.3604V99.5746L82.5588 99.3723C92.426 98.7483 103.433 96.7492 110.711 86.6514C117.09 77.7993 120 63.3375 120 41.1372V0H67.2727Z" fill="url(#paint1_linear_34_175)" fillOpacity="0.46" />
         <defs>
           <linearGradient id="paint0_linear_34_175" x1="26.3636" y1="0" x2="26.3636" y2="99.5746" gradientUnits="userSpaceOnUse">
             <stop />
-            <stop offset="1" stop-color="white" stop-opacity="0.65" />
+            <stop offset="1" stopColor="white" stopOpacity="0.65" />
           </linearGradient>
           <linearGradient id="paint1_linear_34_175" x1="93.6363" y1="0" x2="93.6363" y2="99.5746" gradientUnits="userSpaceOnUse">
             <stop />
-            <stop offset="1" stop-color="white" stop-opacity="0.65" />
+            <stop offset="1" stopColor="white" stopOpacity="0.65" />
           </linearGradient>
         </defs>
       </svg>
