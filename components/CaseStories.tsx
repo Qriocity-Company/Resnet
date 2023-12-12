cdd0e0b058ea02f18696f5a8aa8de1ae3a1e6070
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-12 21:44:54 +0530

    bug fixes

--- components/CaseStories.tsx
+++ components/CaseStories.tsx
@@ -187,11 +187,11 @@ export const CaseStories = () => {
                 <div className='w-full md:w-[60%] '>
                   <h1 className='mt-2 md:mt-0 text-2xl lg:text-3xl font-bold text-transparent bg-gradient-to-r from-[#FF001D] from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text'>Redefining Student Projects</h1>
                   <p className='mt-3 text-justify lg:text-xl w-[100%]'>
-                  ResNet's tailored solutions addressed the intricacies of complex project challenges, providing students with a pathway to navigate academic pressures and job placement stress seamlessly. 
+                  ResNet&apos;s tailored solutions addressed the intricacies of complex project challenges, providing students with a pathway to navigate academic pressures and job placement stress seamlessly. 
                   </p>
 
-                  <div>
-                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>ResNet's Impact on Student Projects</h2>
+                  <div className='hidden md:block lg:text-xl'>
+                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>ResNet&apos;s Impact on Student Projects</h2>
                     <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
                       <li>Streamlined processes and personalized guidance.</li>
                       <li>Significantly reduced project completion time by 30%.</li>
@@ -201,7 +201,7 @@ export const CaseStories = () => {
                     <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Qriocity.in: Beyond Academics with ResNet</h2>
                     <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
                       <li>ResNet plays a pivotal role in streamlining projects.</li>
-                      <li>Offers enticing group discounts and introduces the 'Earn While You Learn' program.</li>
+                      <li>Offers enticing group discounts and introduces the Earn While You Learn program.</li>
                       <li>Strategic approach results in a remarkable 25% increase in project participation.</li>
                       <li>Heightened level of student engagement at Qriocity.in.</li>
                     </ul>
@@ -237,19 +237,20 @@ export const CaseStories = () => {
                   <p className='mt-3 text-justify lg:text-xl w-[100%]'>
                   Witness a substantial reduction in manual efforts as AI-powered automation streamlines labor-intensive tasks, enhancing efficiency in document creation, editing, and distribution.
                   </p>
+                  <div className='md:block hidden lg:text-xl'>
+                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Efficiency Amplified Through AI</h2>
+                    <ul className='mt-2 text-justify w-[100%] list-disc pl-6'>
+                      <li>Discover the transformative impact of AI as ResNet streamlines processes, provides scalability, and reduces reliance on manual expertise.</li>
+                      <li>Significantly reduced Doucmentation time by 40%.</li>
+                      
+                    </ul>
 
-                  <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Efficiency Amplified Through AI</h2>
-                  <ul className='mt-2 text-justify w-[100%] list-disc pl-6'>
-                    <li>Discover the transformative impact of AI as ResNet streamlines processes, provides scalability, and reduces reliance on manual expertise.</li>
-                    <li>Significantly reduced Doucmentation time by 40%.</li>
-                    
-                  </ul>
-
-                  <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Minimized Documentation Time and Errors</h2>
-                  <ul className='mt-2 text-justify w-[100%] list-disc pl-6'>
-                    <li>Witness a revolutionary reduction in documentation time and errors as ResNet's AI-driven automation ensures precision and accuracy throughout the process.</li>
-                    <li>This success story underlines ResNet's commitment to excellence, showcasing how our solutions minimize documentation time, mitigate errors, and optimize overall resource utilization.</li>
-                  </ul>
+                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Minimized Documentation Time and Errors</h2>
+                    <ul className='mt-2 text-justify w-[100%] list-disc pl-6'>
+                      <li>Witness a revolutionary reduction in documentation time and errors as ResNet&apos;s AI-driven automation ensures precision and accuracy throughout the process.</li>
+                      <li>This success story underlines ResNet&apos;s commitment to excellence, showcasing how our solutions minimize documentation time, mitigate errors, and optimize overall resource utilization.</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
@@ -275,23 +276,24 @@ export const CaseStories = () => {
               <div className={`block md:flex gap-5 h-full w-full ${!clicked2? 'max-h-[450px]':''} overflow-hidden `}>
                 <Image src={invictus} alt='Documentation' className='h-[50%] md:h-full w-full md:w-[40%] object-cover'></Image>
                 <div className='w-full md:w-[60%] '>
-                  <h1 className='mt-1 md:mt-0 text-2xl lg:text-3xl font-bold text-transparent bg-gradient-to-r from-[#FF001D] from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text'>ResNet's Influence on Digital Marketing</h1>
+                  <h1 className='mt-1 md:mt-0 text-2xl lg:text-3xl font-bold text-transparent bg-gradient-to-r from-[#FF001D] from-40% via-purple-800 via-60% to-blue-700 to-100% bg-clip-text'>ResNet&apos;s Influence on Digital Marketing</h1>
                   <p className='mt-2 md:mt-3 text-justify lg:text-xl w-[100%]'>
-                  Explore how ResNet revolutionized Invictus's digital marketing approach, addressing the need for a robust online presence. Our expertise crafted a visually stunning website, enhancing brand engagement and user experience.
+                  Explore how ResNet revolutionized Invictus&apos;s digital marketing approach, addressing the need for a robust online presence. Our expertise crafted a visually stunning website, enhancing brand engagement and user experience.
                   </p>
+                  <div className='md:block hidden lg:text-xl'>
+                    <h2 className='mt-2 md:mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Navigating Digital Challenges wuth Invictus</h2>
+                    <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
+                      <li>Through ResNet&apos;s strategies, Invictus witnessed a significant increase in website traffic, improved brand perception, and higher conversion rates.</li>
+                      <li>Discover the impactful partnership that tackled the complexities of online visibility and brand perception.</li>
+                      
+                    </ul>
 
-                  <h2 className='mt-2 md:mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Navigating Digital Challenges wuth Invictus</h2>
-                  <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
-                    <li>Through ResNet's strategies, Invictus witnessed a significant increase in website traffic, improved brand perception, and higher conversion rates.</li>
-                    <li>Discover the impactful partnership that tackled the complexities of online visibility and brand perception.</li>
-                    
-                  </ul>
-
-                  <h2 className='mt-2 md:mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Achieving Digital Dominance</h2>
-                  <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
-                    <li>ResNet's expertise propelled Invictus to digital dominance.</li>
-                    <li>Achievements include a visually appealing and highly functional website, elevated online visibility, and a growing online presence.</li>
-                  </ul>
+                    <h2 className='mt-2 md:mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Achieving Digital Dominance</h2>
+                    <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
+                      <li>ResNet&apos;s expertise propelled Invictus to digital dominance.</li>
+                      <li>Achievements include a visually appealing and highly functional website, elevated online visibility, and a growing online presence.</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
 
@@ -321,19 +323,20 @@ export const CaseStories = () => {
                   <p className='mt-1 md:mt-3 text-justify lg:text-xl w-[100%]'>
                   ResNet spearheaded a transformative shift in customer data analytics by introducing an AI-powered solution that automated data processing, extracted actionable insights, and delivered personalized recommendations.
                   </p>
+                  <div className='md:block hidden lg:text-xl'>
+                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Strategic Implementation and Overcoming Challenges</h2>
+                    <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
+                      <li>ResNet strategically executed the implementation of advanced analytics algorithms ensuring data privacy, and addressing skepticism.</li>
+                      <li>Marked a paradigm shift towards informed decision-making in customer data analytics.</li>
+                      
+                    </ul>
 
-                  <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Strategic Implementation and Overcoming Challenges</h2>
-                  <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
-                    <li>ResNet strategically executed the implementation of advanced analytics algorithms ensuring data privacy, and addressing skepticism.</li>
-                    <li>Marked a paradigm shift towards informed decision-making in customer data analytics.</li>
-                    
-                  </ul>
-
-                  <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Strategic Implementation</h2>
-                  <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
-                    <li>The power of advanced analytics, fostering unparalleled customer engagement and driving informed decision-making.</li>
-                    <li>The profound impact of this initiative manifested in a remarkable 25% improvement in customer satisfaction and overall business performance.</li>
-                  </ul>
+                    <h2 className='mt-3 text-left text-base lg:text-2xl font-bold text-[#FF001D]'>Strategic Implementation</h2>
+                    <ul className='mt-2 text-justify  w-[100%] list-disc pl-6'>
+                      <li>The power of advanced analytics, fostering unparalleled customer engagement and driving informed decision-making.</li>
+                      <li>The profound impact of this initiative manifested in a remarkable 25% improvement in customer satisfaction and overall business performance.</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
 
