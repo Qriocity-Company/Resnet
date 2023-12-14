71470ce0213781ff657aa0a92d2e55f8b40d852f
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-14 18:50:02 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11433484632"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-11433484632');
-            `,
-          }}
-          
-        />
-      </Head>
      
       <body className='w-full text-gray-200 bg-[#08081B] flex flex-col justify-center items-center ' >{children}</body>
     </html>
