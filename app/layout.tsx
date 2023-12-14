d695f1f5aea9052a7551fc24748c51858ec3e1fb
thelostwarrior69 <91337266+AdityaCoder-N@users.noreply.github.com>
2023-12-14 16:58:34 +0530

    Added google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import Head from 'next/head'
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +8,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11433484632"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11433484632');
+            `,
+          }}
+          
+        />
+      </Head>
+     
       <body className='w-full text-gray-200 bg-[#08081B] flex flex-col justify-center items-center ' >{children}</body>
     </html>
   )
